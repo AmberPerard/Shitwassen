406638ba46a03e539cebb99fca3130edb4057ce4
Moongirl1207 <amber.perard@student.howest.be>
2021-01-31 12:20:59 +0100

    First Github Commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject MoveTshirt;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      MoveTshirt.transform.Translate(Vector3.right * speed/100);
+
+     if (MoveTshirt.transform.position.x >= 4.2)
+        {
+            MoveTshirt.transform.position = new Vector3(4.2f, MoveTshirt.transform.position.y, MoveTshirt.transform.position.z );
+        }
+     if (MoveTshirt.transform.position.x <= -4.2)
+        {
+            MoveTshirt.transform.position = new Vector3(-4.2f, MoveTshirt.transform.position.y, MoveTshirt.transform.position.z );
+        }
+    }
+}
---
+++ Assets/Scripts/SpeedCalculate.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedCalculate : MonoBehaviour
+{
+
+    private float timer = 5f;
+    private int P1Clicks;
+    private int P2Clicks;
+
+    public GameController gameController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer < 1)
+        {
+            if (Input.GetKeyDown("l")) P1Clicks++;
+            if (Input.GetKeyDown("k")) P2Clicks++;
+        }
+        else
+        {
+            Debug.Log("I had " + P1Clicks + " in that last second.");
+            Debug.Log("I had " + P2Clicks + " in that last second.");
+
+            int speedAddative = P1Clicks - P2Clicks;
+            Debug.Log(speedAddative);
+            gameController.speed = speedAddative/2;
+
+            P2Clicks = 0;
+            P1Clicks = 0;
+            timer = 0f;
+        }
+    }
+}
